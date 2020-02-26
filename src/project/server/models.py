6e07bf7b0aa9b79322c2a5488a958b225d3b6885
Ying Wang <yingw787.personal@gmail.com>
2020-02-26 18:19:59 -0500

    Update models.py add columns and init.

--- src/project/server/models.py
+++ src/project/server/models.py
@@ -9,3 +9,36 @@ class User(db.Model):
     """User Model for storing user-related details.
     """
     __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    email = db.Column(
+        db.String(255),
+        unique=True,
+        nullable=False
+    )
+    password = db.Column(
+        db.String(255),
+        nullable=False
+    )
+    registered_on = db.Column(
+        db.DateTime,
+        nullable=False
+    )
+    admin = db.Column(
+        db.Boolean,
+        nullable=False,
+        default=False
+    )
+
+    def __init__(self, email, password, admin=False):
+        self.email = email
+        self.password = bcrypt.generate_password_hash(
+            password,
+            app.config.get('BCRYPT_LOG_ROUNDS')
+        ).decode()
+        self.registered_on = datetime.datetime.now()
+        self.admin = admin
