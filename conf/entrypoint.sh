8385795ae202d9b221c83e74ab0a55d49937889d
Ying Wang <yingw787.personal@gmail.com>
2020-02-26 14:23:18 -0500

    Update entrypoint.sh create Python venv environment when scripting Python environment setup.

--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -1,6 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # Run after Docker build process has finished, and runtime configuration begins.
+
+# Install persisted dependencies in venv at '/root/env'.
+#
+# NOTE: Must take place as one context since runtime context isn't preserved
+# across different Docker build layers.
+$(which python3.6) -m venv ${HOME}/env && \
+    source ${HOME}/env/bin/activate && \
+    pip3 install wheel && \
+    pip3 install -r /src/requirements.txt
 
 # Keep the container running.
 tail -f /dev/null
