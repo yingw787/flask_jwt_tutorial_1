35b3eceff39853b929b96614710c55bfe7c3511b
Ying Wang <yingw787.personal@gmail.com>
2020-02-27 12:59:03 -0500

    Update test_auth.py add one integration test.

--- src/project/tests/test_auth.py
+++ src/project/tests/test_auth.py
@@ -1,5 +1,6 @@
 # project/tests/test_auth.py
 
+import json
 import unittest
 
 from project.server import db
@@ -8,7 +9,26 @@ from project.tests.base import BaseTestCase
 
 
 class TestAuthBlueprint(BaseTestCase):
-    pass
+
+    def test_registration(self):
+        """
+        Test for user registration.
+        """
+        with self.client:
+            response = self.client.post(
+                '/auth/register',
+                data=json.dumps(dict(
+                    email='joe@gmail.com',
+                    password='123456'
+                )),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'success')
+            self.assertTrue(data['message'] == 'Successfully registered.')
+            self.assertTrue(data['auth_token'])
+            self.assertTrue(response.content_type == 'application/json')
+            self.assertEqual(response.status_code, 201)
 
 
 if __name__=='__main__':
