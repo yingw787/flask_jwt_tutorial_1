f56297d903c4248a51085d35d3059ecf174bd446
Ying Wang <yingw787.personal@gmail.com>
2020-02-27 12:49:45 -0500

    Add auth/ directory for authentication routing.

---
+++ src/project/server/auth/views.py
@@ -0,0 +1,10 @@
+# project/server/auth/views.py
+
+from flask import Blueprint, request, make_response, jsonify
+from flask.views import MethodView
+
+from project.server import bcrypt, db
+from project.server.models import User
+
+
+auth_blueprint = Blueprint('auth', __name__)
