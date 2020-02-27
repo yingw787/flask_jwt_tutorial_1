dea4adacd73ddf63d77bcf8f3a8a13e3aabfcee2
Ying Wang <yingw787.personal@gmail.com>
2020-02-27 12:52:57 -0500

    Add test_auth.py scaffolding for testing auth routes.

---
+++ src/project/tests/test_auth.py
@@ -0,0 +1,15 @@
+# project/tests/test_auth.py
+
+import unittest
+
+from project.server import db
+from project.server.models import User
+from project.tests.base import BaseTestCase
+
+
+class TestAuthBlueprint(BaseTestCase):
+    pass
+
+
+if __name__=='__main__':
+    unittest.main()
