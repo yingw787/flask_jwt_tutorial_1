9674bb5155b8662f6593d279e1cb272a71ac09d2
Ying Wang <yingw787.personal@gmail.com>
2020-02-27 18:14:12 -0500

    Update views.py add LogoutAPI resource and view.

--- src/project/server/auth/views.py
+++ src/project/server/auth/views.py
@@ -4,7 +4,7 @@ from flask import Blueprint, request, make_response, jsonify
 from flask.views import MethodView
 
 from project.server import bcrypt, db
-from project.server.models import User
+from project.server.models import User, BlacklistToken
 
 
 auth_blueprint = Blueprint('auth', __name__)
@@ -153,4 +153,57 @@ auth_blueprint.add_url_rule(
     '/auth/status',
     view_func=user_view,
     methods=['GET']
+)
+
+
+class LogoutAPI(MethodView):
+    """
+    Logout Resource
+    """
+    def post(self):
+        # get auth token
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split(" ")[1]
+        else:
+            auth_token = ''
+        if auth_token:
+            resp = User.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                # mark the token as blacklisted
+                blacklist_token = BlacklistToken(token=auth_token)
+                try:
+                    # insert the token
+                    db.session.add(blacklist_token)
+                    db.session.commit()
+                    responseObject = {
+                        'status': 'success',
+                        'message': 'Successfully logged out.'
+                    }
+                    return make_response(jsonify(responseObject)), 200
+                except Exception as e:
+                    responseObject = {
+                        'status': 'fail',
+                        'message': e
+                    }
+                    return make_response(jsonify(responseObject)), 200
+            else:
+                responseObject = {
+                    'status': 'fail',
+                    'message': resp
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return make_response(jsonify(responseObject)), 403
+
+logout_view = LogoutAPI.as_view('logout_api')
+
+auth_blueprint.add_url_rule(
+    '/auth/logout',
+    view_func=logout_view,
+    methods=['POST']
 )
