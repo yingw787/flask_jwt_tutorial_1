528ee8b9c8268114c2d96f734b05600ab7b6ba60
Ying Wang <yingw787.personal@gmail.com>
2020-02-26 19:24:29 -0500

    Update entrypoint.sh create python3 virtualenv and install dependencies to make directory exist at runtime

--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -2,6 +2,27 @@
 #
 # Run after Docker build process has finished, and runtime configuration begins.
 
+# Create virtualenv from requirements.txt.
+#
+# NOTE: I tried to create this via Dockerfile, but the directory keeps
+# disappearing. I'm not sure why. Tutorial installs a bunch of new dependencies,
+# and I'm using PostgreSQL 12.x which at least breaks SQLAlchemy version b/w
+# tutorial, so I need to stand up infra idempotently, which means I need this
+# part working idempotently.
+
+echo "Creating virtualenv as $(whoami)." >> /tmp/log.txt
+mkdir -p /root/env
+python3 -m venv /root/env
+source /root/env/bin/activate
+
+echo "Installing requirements with python3 as $(which python3)." >> /tmp/log.txt
+pip3 install wheel
+pip3 install -r /root/requirements.txt
+
+echo "\n" >> /tmp/log.txt
+echo "Running tests." >> /tmp/log.txt
+# Can't redirect python3 stdout to file w/o source mutation.
+
 # Start PostgreSQL instance.
 service postgresql start
 
