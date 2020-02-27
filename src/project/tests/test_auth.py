53bfe21c8ad827d5d64be62b868606b70b99d6b5
Ying Wang <yingw787.personal@gmail.com>
2020-02-27 13:20:09 -0500

    Update test_auth.pyadd one integration test.

--- src/project/tests/test_auth.py
+++ src/project/tests/test_auth.py
@@ -30,6 +30,33 @@ class TestAuthBlueprint(BaseTestCase):
             self.assertTrue(response.content_type == 'application/json')
             self.assertEqual(response.status_code, 201)
 
+    def test_registered_with_already_registered_user(self):
+        """
+        Test registration with already registered email
+        """
+        user = User(
+            email='joe@gmail.com',
+            password='test'
+        )
+        db.session.add(user)
+        db.session.commit()
+        with self.client:
+            response = self.client.post(
+                '/auth/register',
+                data=json.dumps(dict(
+                    email='joe@gmail.com',
+                    password='123456'
+                )),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'fail')
+            self.assertTrue(
+                data['message'] == 'User already exists. Please Log in.'
+            )
+            self.assertTrue(response.content_type == 'application/json')
+            self.assertEqual(response.status_code, 202)
+
 
 if __name__=='__main__':
     unittest.main()
