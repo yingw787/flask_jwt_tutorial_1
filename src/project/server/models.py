f1267f52ec414a39a9ed481af6df27b724b59671
Ying Wang <yingw787.personal@gmail.com>
2020-02-27 18:32:35 -0500

    Update models.py add method to check blacklisted token and send 401 if token has been blacklisted.

--- src/project/server/models.py
+++ src/project/server/models.py
@@ -76,7 +76,11 @@ class User(db.Model):
                 auth_token,
                 app.config.get('SECRET_KEY')
             )
-            return payload['sub']
+            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
+            if is_blacklisted_token:
+                return 'Token blacklisted. Please log in again.'
+            else:
+                return payload['sub']
         except jwt.ExpiredSignatureError:
             return 'Signature expired. Please log in again.'
         except jwt.InvalidTokenError:
@@ -110,3 +114,13 @@ class BlacklistToken(db.Model):
 
     def __repr__(self):
         return '<id : token: {}'.format(self.token)
+
+    @staticmethod
+    def check_blacklist(auth_token):
+        # check whether auth token has been blacklisted
+        res = BlacklistToken.query.filter_by(token=str(auth_token)).first()
+        return (
+            True
+            if res
+            else False
+        )
