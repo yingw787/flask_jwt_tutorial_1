35f7b56838b34ae96040dc70b2f6a5ec482c3cf7
Ying Wang <yingw787.personal@gmail.com>
2020-02-26 19:32:54 -0500

    Update entrypoint.sh templating parts of tutorial.

--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -13,15 +13,27 @@
 echo "Creating virtualenv as $(whoami)." >> /tmp/log.txt
 mkdir -p /root/env
 python3 -m venv /root/env
-source /root/env/bin/activate
 
 echo "Installing requirements with python3 as $(which python3)." >> /tmp/log.txt
-pip3 install wheel
-pip3 install -r /root/requirements.txt
+
+source /root/env/bin/activate && \
+    pip3 install wheel && \
+    pip3 install -r /root/requirements.txt
 
 echo "\n" >> /tmp/log.txt
 echo "Running tests." >> /tmp/log.txt
 # Can't redirect python3 stdout to file w/o source mutation.
+
+echo "\n" >> /tmp/log.txt
+echo "Running migration." >> /tmp/log.txt
+
+# This section of code apparently needs to be run manually, not sure why it
+# doesn't work while scripted but I copied and pasted this into the Docker
+# context and the PostgreSQL instance migrated successfully.
+source /root/env/bin/activate && \
+    PGPASSWORD=postgres python3 /root/manage.py create_db || true && \
+    PGPASSWORD=postgres python3 /root/manage.py db init || true && \
+    PGPASSWORD=postgres python3 /root/manage.py db migrate
 
 # Start PostgreSQL instance.
 service postgresql start
