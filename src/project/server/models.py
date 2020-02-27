c943dc71ddc0b91964c1b9332688b5c9ec5788f1
Ying Wang <yingw787.personal@gmail.com>
2020-02-27 17:10:08 -0500

    Update models.py add BlacklistToken.

--- src/project/server/models.py
+++ src/project/server/models.py
@@ -81,3 +81,32 @@ class User(db.Model):
             return 'Signature expired. Please log in again.'
         except jwt.InvalidTokenError:
             return 'Invalid token. Please log in again.'
+
+
+class BlacklistToken(db.Model):
+    """
+    Token Model for storing JWT tokens
+    """
+    __tablename__ = 'blacklist_tokens'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    token = db.Column(
+        db.String(500),
+        unique=True,
+        nullable=False
+    )
+    blacklisted_on = db.Column(
+        db.DateTime,
+        nullable=False
+    )
+
+    def __init__(self, token):
+        self.token = token
+        self.blacklisted_on = datetime.datetime.now()
+
+    def __repr__(self):
+        return '<id : token: {}'.format(self.token)
