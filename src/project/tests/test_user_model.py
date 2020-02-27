98fc4cd676b24126a17b4b05d520af328b50c4f6
Ying Wang <yingw787.personal@gmail.com>
2020-02-27 11:09:59 -0500

    Update test_user_model.py add one test.

---
+++ src/project/tests/test_user_model.py
@@ -0,0 +1,24 @@
+# project/tests/test_user-model.py
+
+import unittest
+
+from project.server import db
+from project.server.models import User
+from project.tests.base import BaseTestCase
+
+
+class TestUserModel(BaseTestCase):
+
+    def test_encode_auth_token(self):
+        user = User(
+            email='test@test.com',
+            password='test'
+        )
+        db.session.add(user)
+        db.session.commit()
+        auth_token = user.encode_auth_token(user.id)
+        self.assertTrue(isinstance(auth_token, bytes))
+
+
+if __name__=='__main__':
+    unittest.main()
